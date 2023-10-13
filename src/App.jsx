e730888466a001dccad7b893ca0764ea183c9b9b
AlexisNtec <ferney.naranjo@novatec.com.co>
2023-10-12 21:47:32 -0500

    rutas y reservas con notificación finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route, Switch } from "wouter";
+import { Toaster } from "react-hot-toast";
+import HotelList from "./components/HotelList";
+import HotelDetails from "./components/HotelDetails";
+
+
+const client = new QueryClient();
+
 function App() {
     
   return(
-    <h1>Hola Mundo</h1>
+    <>
+      <Toaster position="top-left" reverseOrder={false}/>
+      <QueryClientProvider client={client}>
+        <Switch>
+          <Route path="/" component={HotelList}/>
+          <Route path="/hotel/:id" component={HotelDetails}/>
+        </Switch>
+      </QueryClientProvider> 
+    </>
+
   )
   
 }
